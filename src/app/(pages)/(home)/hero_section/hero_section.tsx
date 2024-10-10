a045a7538e7ff2b9dbcd9c84f66d62f76a60c823
Yamin Nather <2001s.yn@gmail.com>
2024-10-10 11:26:27 +0530

    =Update Hero Section responsiveness

--- src/app/(pages)/(home)/hero_section/hero_section.tsx
+++ src/app/(pages)/(home)/hero_section/hero_section.tsx
@@ -10,15 +10,16 @@ import { useIsVisible } from "@/app/hooks/use_is_visible/use_is_visible";
 
 export function HeroSection(): ReactNode {
   const [sectionElement, setSectionElement] = useState<HTMLElement | null>(null);
-  const isVisible = useIsVisible(sectionElement);
+  // const isVisible = useIsVisible(sectionElement);
+  const isVisible = true;
 
   return (
     <section 
-      ref={(element) => setSectionElement(element)} 
+      ref={(element) => setSectionElement(element)}
       id="hero" 
-      className={`flex py-16 items-center`}
+      className={`grid p-4 py-16 grid-cols-1 md:grid-cols-[90fr,10fr] gap-8 text-center md:text-start`}
     >
-      <div className={`w-8/12 animate-slide-in-y-initial ${(isVisible) ? 'animate-slide-in-y' : ''}`}>
+      <div className={`animate-slide-in-y-initial ${(isVisible) ? 'animate-slide-in-y' : ''}`}>
         <p className="font-mono text-5xl font-bold text-white">Hi👋, I&apos;m Yamin Nather</p>
         
         <p className="mt-8">
@@ -30,14 +31,13 @@ export function HeroSection(): ReactNode {
       
         <p>I&apos;m very passionate about coding and actively involved in extracurricular activities, demonstrating strong teamwork and leadership skills.</p>
         
-        <div className="mt-8 flex flex-wrap">
+        <div className="mt-8 inline-flex flex-wrap">
           {ExternalLink.values.map(
             (element, index) => <ExternalLinkLink key={element.name} icon={externalLinkIcons.get(element)!} name={element.name} url={element.url} className={(index !== 0) ? 'ms-4' : undefined} />
           )}
         </div>
       </div>
 
-      <div className="w-1/12" />
       
       <div className={`animate-slide-in-y-initial animate-delay-200 ${(isVisible) ? 'animate-slide-in-y' : ''}`}>
         <AbstractAnimation />
