e1e3a9a535a5e252e1424dff22265dcee1851b69
Yamin Nather <2001s.yn@gmail.com>
2025-04-08 19:31:35 +0530

    removed resume

--- src/app/(pages)/(home)/hero_section/external_link.tsx
+++ src/app/(pages)/(home)/hero_section/external_link.tsx
@@ -6,11 +6,11 @@ export class ExternalLink {
 
   static readonly github = new ExternalLink('Github', "https://github.com/YaminNather", (className) => <FaGithub key="github" className={className} />);
   static readonly linkedin = new ExternalLink('Linkedin', "https://linkedin.com/in/yamin-nather", (className) => <FaLinkedin key="linkedin" className={className} />);
-  static readonly resume = new ExternalLink('Resume', "https://drive.google.com/file/d/1gGUfZHk6l5KNV51tEPDkKcScq5UwC2ow/view?usp=sharing", (className) => <FaFileAlt key="resume" className={className} />);
+  // static readonly resume = new ExternalLink('Resume', "https://drive.google.com/file/d/1gGUfZHk6l5KNV51tEPDkKcScq5UwC2ow/view?usp=sharing", (className) => <FaFileAlt key="resume" className={className} />);
   
   static readonly values: ExternalLink[] = [
     ExternalLink.github, 
     ExternalLink.linkedin,
-    ExternalLink.resume,
+    // ExternalLink.resume,
   ];
 }
